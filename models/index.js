f651595f3923eab0c29e7af8f395517902aa995e
Savannah Kolb <savannahnkolb@gmail.com>
2021-06-26 19:48:34 -0400

    set up models and server

--- models/index.js
+++ models/index.js
@@ -5,12 +5,34 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+              // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+            // Define an alias for when data is retrieved
+  as: 'product_tag1'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'tag_product2'
+});
+
 
 module.exports = {
   Product,
@@ -18,3 +40,6 @@ module.exports = {
   Tag,
   ProductTag,
 };
+
+
+// onDelete: 'CASCADE' Deletes everything associated with this id
