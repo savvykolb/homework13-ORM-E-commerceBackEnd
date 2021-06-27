120f43ce6ce43269ae945bde2a9e12afbf1fb50c
Savannah Kolb <savannahnkolb@gmail.com>
2021-06-26 20:33:02 -0400

    updated category routies - except PUT

--- server.js
+++ server.js
@@ -11,11 +11,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
 
 
-// sequelize.sync({ force: false }).then(() => {
-//   app.listen(PORT, () => console.log('Now listening'));
+// app.listen(PORT, () => {
+//   console.log(`App listening on port ${PORT}!`);
 // });
+
